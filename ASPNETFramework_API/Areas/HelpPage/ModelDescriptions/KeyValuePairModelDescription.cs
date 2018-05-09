495761d57e4f05518d6c2e5249a4198e42e39813
Amit Saha <amitsaha.in@gmail.com>
2018-05-09 16:09:15 +1000

    initial commit

---
+++ ASPNETFramework_API/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace ASPNETFramework_API.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ ASPNETFramework_API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASPNETFramework_API.WebApiApplication" Language="C#" %>
