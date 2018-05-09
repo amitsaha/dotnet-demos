16b4ffd439ff6781b84b67bf5df12f4ad87d4d37
Amit Saha <amitsaha.in@gmail.com>
2018-05-09 16:12:14 +1000

    update

--- demo1/ASPNETFramework_API/Controllers/ValuesController.cs
+++ demo1/ASPNETFramework_API/Controllers/ValuesController.cs
@@ -12,7 +12,7 @@ namespace ASPNETFramework_API.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value8" };
+            return new string[] { "value1", "value9" };
         }
 
         // GET api/values/5
