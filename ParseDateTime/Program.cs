571b59cfd55acb92f4abca8e293f7dcb89413569
Amit Saha <amitsaha.in@gmail.com>
2018-06-14 17:32:55 +1000

    new demo

---
+++ ParseDateTime/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace ParseDateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dateString;  
+            DateTimeOffset result;
+            // Parse date and time and convert to UTC.
+            dateString = "Fri, 15 Jun 2018 00:30:49 GMT"; 
+           
+            if (DateTimeOffset.TryParseExact(dateString, "r", (IFormatProvider)CultureInfo.InvariantCulture, DateTimeStyles.RoundtripKind, out result))
+                Console.WriteLine("'{0}' converts to {1}.", dateString, result.ToString());
+            else
+                Console.WriteLine("'{0}' is not in the correct format.", dateString);
+        }
+    }
+}
