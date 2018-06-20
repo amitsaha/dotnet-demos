aa5d279dcaa64f2ccd91c4ed74174c4092532b91
Amit Saha <amitsaha.in@gmail.com>
2018-06-20 18:21:44 +1000

    docker+csharp

---
+++ docker_dircp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Docker.DotNet;
+using Docker.DotNet.Models;
+
+namespace docker_dircp
+{
+    class Program
+    {
+        static async Task<IList<ContainerListResponse>> ListContainersAsync(DockerClient client)
+        {
+            Console.WriteLine("Listing containers");           
+            return await client.Containers.ListContainersAsync(new ContainersListParameters() { Limit = 10, });
+        }
+
+        static void ListContainersProxy(DockerClient client)
+        {
+           var containers = ListContainersAsync(client);
+
+           foreach (var c in containers.Result)
+           {
+                Console.WriteLine(c.Names[0]);
+           }
+           
+        }           
+
+
+        static void Main(string[] args)
+        {            
+            DockerClient client = new DockerClientConfiguration(new Uri("npipe://./pipe/docker_engine")).CreateClient();
+            ListContainersProxy(client);
+            
+        }
+    }
+}
