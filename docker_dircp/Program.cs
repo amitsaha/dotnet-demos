8dc32bdc694f4b3df8ebab6678788f8cb71264db
Amit Saha <amitsaha.in@gmail.com>
2018-06-21 17:15:31 +1000

    update

--- docker_dircp/Program.cs
+++ docker_dircp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 using Docker.DotNet;
 using Docker.DotNet.Models;
@@ -10,20 +11,63 @@ namespace docker_dircp
     {
         static async Task<IList<ContainerListResponse>> ListContainersAsync(DockerClient client)
         {
-            Console.WriteLine("Listing containers");           
-            return await client.Containers.ListContainersAsync(new ContainersListParameters() { Limit = 10, });            
-            
-        }           
+            return await client.Containers.ListContainersAsync(new ContainersListParameters() { Limit = 10, });
 
+        }
 
-        static void Main(string[] args)
-        {            
-            DockerClient client = new DockerClientConfiguration(new Uri("npipe://./pipe/docker_engine")).CreateClient();
+        static ContainerListResponse CheckContainerExists(DockerClient client, String ContainerId)
+        {
             var containers = ListContainersAsync(client);
             foreach (var c in containers.Result)
             {
-                Console.WriteLine(c.Names[0]);
+                // Reliable enough?
+                if (c.ID.StartsWith(ContainerId))
+                {
+                    return c;
+                }
             }
+            return null;
+        }
+
+        static void CopyDirectoryFromContainer(DockerClient client, String ContainerId, String DirToCopy)
+        {
+            var container = CheckContainerExists(client, ContainerId);
+
+            if (container == null)
+            {
+                Console.WriteLine("Container doesn't exist");
+            }
+            else
+            {
+                GetArchiveFromContainerParameters ArchiveParameters = new GetArchiveFromContainerParameters
+                {
+                    Path = DirToCopy
+                };
+                try
+                {
+                    var TaskResult = client.Containers.GetArchiveFromContainerAsync(container.ID, ArchiveParameters, false);
+                    var data = TaskResult.Result.Stream;
+
+                    MemoryStream ms = new MemoryStream();
+                    data.CopyTo(ms);
+                    ms.Dispose();
+
+                    var DestFilename = Path.GetFileName(DirToCopy) + ".tar";
+                    Console.WriteLine("Writing: " + DestFilename);
+                    File.WriteAllBytes(DestFilename, ms.ToArray());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error when retrieving files: " + e.Message);
+                }
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            DockerClient client = new DockerClientConfiguration(new Uri("npipe://./pipe/docker_engine")).CreateClient();
+            CopyDirectoryFromContainer(client, args[0], args[1]);
 
         }
     }
