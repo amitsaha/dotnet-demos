52e34a4fe06c788fe879bc370f5882697d1baf5e
Amit Saha <amitsaha.in@gmail.com>
2018-06-27 10:20:22 +1000

    update

--- ParseDateTime/Program.cs
+++ ParseDateTime/Program.cs
@@ -9,6 +9,7 @@ namespace ParseDateTime
         {
             string dateString;  
             DateTimeOffset result;
+
             // Parse date and time and convert to UTC.
             dateString = "Fri, 15 Jun 2018 00:30:49 GMT"; 
            
@@ -16,6 +17,13 @@ namespace ParseDateTime
                 Console.WriteLine("'{0}' converts to {1}.", dateString, result.ToString());
             else
                 Console.WriteLine("'{0}' is not in the correct format.", dateString);
+
+            // Get current date/time in RFC1123 format
+            DateTime now = DateTime.Now;
+            Console.WriteLine("Current datetime now: " +  now.ToString("R"));
+            Console.WriteLine("Current datetime after 7 days: " + now.AddDays(7).ToString("R"));
+
+           // Explore more: http://zetcode.com/articles/csharpdatetime/
         }
     }
 }
