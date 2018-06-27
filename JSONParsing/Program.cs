ce62751cbc0199f3191f4f5a5b49e44b0e26cc68
Amit Saha <amitsaha.in@gmail.com>
2018-06-27 12:48:19 +1000

    json parsing

---
+++ JSONParsing/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace JSONParsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jsonData = @"{""Id"":""06094ea0-1aa4-47b8-9c11-ad18cb18ee72""}";
+            
+           // Deserialize json encoded string to an object
+           var jsonObject = JsonConvert.DeserializeObject(jsonData);
+           Console.WriteLine(jsonObject);
+        }
+    }
+}
