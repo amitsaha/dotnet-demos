e4ed7de1401be92b2d92f576aa08a30654a55541
Amit Saha <amitsaha.in@gmail.com>
2018-08-31 18:15:24 +1000

    AWS SDK demo

---
+++ AWSSDKDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
+
+using System.Collections.Specialized;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+
+
+namespace AWSSDKDemo
+{
+    class Program
+    {
+        static string bucketName;
+        static string keyName;
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            using (AmazonS3Client client = new AmazonS3Client(RegionEndpoint.APSoutheast2))
+            {
+                var response = client.ListBucketsAsync();
+                foreach (S3Bucket bucket in response.Result.Buckets)
+                {
+                    Console.WriteLine("You own Bucket with name: {0}", bucket.BucketName);
+                }
+            }
+        }
+    }
+}
