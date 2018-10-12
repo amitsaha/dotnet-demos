6ba89c52cd8c4ef19d659da95bc0dc4f3d5ea2ff
Amit Saha <amitsaha.in@gmail.com>
2018-10-12 18:04:56 +1100

    Calling external process demo

---
+++ ShellOut/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ShellOut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process myProcess = new Process();
+            myProcess.StartInfo.UseShellExecute = false;
+            myProcess.StartInfo.FileName = args[0];
+            foreach(string a in args)
+            {
+                myProcess.StartInfo.Arguments += a + " ";
+            }
+            myProcess.Start();
+            myProcess.WaitForExit();
+
+            Console.WriteLine("Process exited with exit code: " + myProcess.ExitCode);
+
+            // continue with https://docs.microsoft.com/en-us/dotnet/api/system.diagnostics.process.exitcode?view=netcore-2.1
+        }
+    }
+    
+}
