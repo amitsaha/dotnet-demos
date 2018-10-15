319a4b60dfe8067403abb9ede6014a089b09818c
Amit Saha <amitsaha.in@gmail.com>
2018-10-15 17:56:24 +1100

    update: print ps resource usage

--- ShellOut/Program.cs
+++ ShellOut/Program.cs
@@ -30,6 +30,7 @@ namespace ShellOut
                 
                 myProcess.Start();
                 StreamReader reader = myProcess.StandardOutput;
+
                 do
                 {
                     if (!myProcess.HasExited)
